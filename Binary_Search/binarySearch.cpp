185c2616fd1cff9166a31d09e8c5c30ea2fb4819
TomasCB18 <tomas71742@gmail.com>
2024-06-20 21:04:43 -0400

    falta modularizar en un .h y hacer pruebas para n's grandes

--- Binary_Search/binarySearch.cpp
+++ Binary_Search/binarySearch.cpp
@@ -2,27 +2,42 @@
 #include <cstdlib>
 #include <ctime>
 #include <algorithm>
+#include <chrono>
+#include <random>
 
 using namespace std;
 
-void crearArray(int n, int *A)
+void crearArrayLineal(int n, int *A, int epsilon)
 {
-    for (int i = 0; i < n; i++)
+    A[0] = rand() % 50;
+    for (int i = 1; i < n; i++)
     {
-        A[i] = rand() % 50;
+        A[i] = A[i - 1] + rand() % epsilon;
     }
 }
 
-void sortArray(int n, int *A)
+void crearArrayNormal(int n, int *A, double mean, double stddev)
 {
+    random_device rd;
+    mt19937 gen(rd());
+    normal_distribution<> d(mean, stddev);
+
+    for (int i = 0; i < n; i++)
+    {
+        A[i] = static_cast<int>(d(gen));
+    }
     sort(A, A + n);
-    cout << "arreglo ordenado A = ";
+}
+
+void printArray(int n, int *A)
+{
     for (int i = 0; i < n; i++)
     {
         cout << A[i] << " ";
     }
     cout << endl;
 }
+
 int binarySearch(int *A, int l, int r, int x)
 {
     if (l > r)
@@ -41,19 +56,30 @@ void validarM(int b, int m)
 {
     if (b == -1)
     {
-        cout << "el valor " << m << " no se encuentra en el arreglo " << endl;
+        cout << "El valor " << m << " no se encuentra en el arreglo." << endl;
     }
     else
     {
-        cout << "el valor " << m << "  se encuentra en la posicion " << b + 1 << " del arreglo" << endl;
+        cout << "El valor " << m << " se encuentra en la posición " << b + 1 << " del arreglo." << endl;
     }
+}
+
+void medirTiempoBusqueda(int n, int *A, int m)
+{
+    auto start = chrono::high_resolution_clock::now();
+    int b = binarySearch(A, 0, n - 1, m);
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double, milli> duration = end - start;
+
+    validarM(b, m);
+    cout << "Tiempo de búsqueda: " << duration.count() << " ms" << endl;
 }
 
 int main(int argc, char **argv)
 {
     if (argc != 3)
     {
-        cout << "Entrada debe ser ./binaryS n m " << endl;
+        cout << "Entrada debe ser ./binaryS n m" << endl;
         exit(1);
     }
     srand(time(nullptr));
@@ -61,18 +87,28 @@ int main(int argc, char **argv)
     int n = atoi(argv[1]);
     int m = atoi(argv[2]);
 
-    int *A = new int[n];
-    int left = 0;
-    int right = n - 1;
+    int *A_lineal = new int[n];
+    int *A_normal = new int[n];
 
-    crearArray(n, A);
-    sortArray(n, A);
+    int epsilon = 10;
+    crearArrayLineal(n, A_lineal, epsilon);
+    double mean = 25.0;
+    double stddev = 5.0;
+    crearArrayNormal(n, A_normal, mean, stddev);
 
-    int b = binarySearch(A, left, right, m);
+    cout << "Arreglo Lineal: ";
+    printArray(n, A_lineal);
+    cout << "Arreglo Normal: ";
+    printArray(n, A_normal);
 
-    validarM(b, m);
+    cout << "Búsqueda en Arreglo Lineal:" << endl;
+    medirTiempoBusqueda(n, A_lineal, m);
 
-    delete[] A;
+    cout << "Búsqueda en Arreglo Normal:" << endl;
+    medirTiempoBusqueda(n, A_normal, m);
+
+    delete[] A_lineal;
+    delete[] A_normal;
 
     return 0;
 }
